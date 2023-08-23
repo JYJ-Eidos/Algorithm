b86e5cf8244e0b4e8600b8992da1473075544248
JYJ <121663316+JYJ-Eidos@users.noreply.github.com>
2023-08-23 14:41:48 +0900

    [unrated] Title: 과일 장수, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,6 +1,6 @@
 const solution = function (k, m, score) {
   const sortScore = score.sort((a, b) => b - a);
-  answer = 0;
+  let answer = 0;
   for (let i = 1; i <= score.length / m; i++) {
     answer += sortScore[i * m - 1] * m;
   }
