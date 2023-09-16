d1377803b429ca8fedb3ff00682ed88207564e2a
JYJ <121663316+JYJ-Eidos@users.noreply.github.com>
2023-09-16 15:03:06 +0900

    [unrated] Title: A 강조하기, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
@@ -0,0 +1,11 @@
+const solution = (myString) => {
+  let answer = "";
+  for (let i = 0; i < myString.length; i++) {
+    if (myString[i] === "a" || myString[i] === "A") {
+      answer += myString[i].toUpperCase();
+    } else {
+      answer += myString[i].toLowerCase();
+    }
+  }
+  return answer;
+};
