52d24aeda2259811c98f5e8c3cbe71610a17143b
JYJ <121663316+JYJ-Eidos@users.noreply.github.com>
2023-09-24 11:27:29 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.31 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -0,0 +1,9 @@
+const solution = (my_string) => {
+  let answer = "";
+  for (let i = 0; i < my_string.length; i++) {
+    my_string[i] === my_string[i].toUpperCase()
+      ? (answer += my_string[i].toLowerCase())
+      : (answer += my_string[i].toUpperCase());
+  }
+  return answer;
+};
