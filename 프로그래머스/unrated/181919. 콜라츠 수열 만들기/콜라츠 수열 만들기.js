60c8ee1aae42a24be374107bc82ed65f97dc7c8f
JYJ <121663316+JYJ-Eidos@users.noreply.github.com>
2023-09-24 11:58:01 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,13 @@
+const solution = (n) => {
+  let answer = [n];
+  while (n !== 1) {
+    if (n % 2 === 0) {
+      n /= 2;
+      answer.push(n);
+    } else {
+      n = 3 * n + 1;
+      answer.push(n);
+    }
+  }
+  return answer;
+};
