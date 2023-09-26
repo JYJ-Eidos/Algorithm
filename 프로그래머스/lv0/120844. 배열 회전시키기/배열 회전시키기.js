391bbade7cf574fbb61e492c6668fea1d932c077
JYJ <121663316+JYJ-Eidos@users.noreply.github.com>
2023-09-26 12:55:26 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -0,0 +1,9 @@
+const solution = (numbers, direction) => {
+  if (direction === "right") {
+    numbers.unshift(numbers.pop());
+    return numbers;
+  } else {
+    numbers.push(numbers.shift());
+    return numbers;
+  }
+};
