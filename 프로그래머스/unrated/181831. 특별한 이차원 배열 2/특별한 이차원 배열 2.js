625da1718a53433d51822381ceea41365d7a27d8
JYJ <121663316+JYJ-Eidos@users.noreply.github.com>
2023-09-29 14:55:42 +0900

    [unrated] Title: 특별한 이차원 배열 2, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -0,0 +1,11 @@
+const solution = (arr) => {
+  const length = arr.length;
+  for (let i = 0; i < length; i++) {
+    for (let j = 1; j < length; j++) {
+      if (arr[i][j] !== arr[j][i]) {
+        return 0;
+      }
+    }
+  }
+  return 1;
+};
