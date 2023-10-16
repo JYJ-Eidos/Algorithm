0f2021bde15c99035547726eb42550f29bf03fb1
JYJ <121663316+JYJ-Eidos@users.noreply.github.com>
2023-10-16 20:48:31 +0900

    [level unrated] Title: 문자열 계산하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120902. 문자열 계산하기/문자열 계산하기.js
@@ -0,0 +1,9 @@
+const solution = (my_string) => {
+  const arr = my_string.split(" ");
+  let answer = Number(arr[0]);
+
+  for (let i = 1; i < arr.length; i += 2) {
+    answer += Number(arr[i] + "1") * Number(arr[i + 1]);
+  }
+  return answer;
+};
