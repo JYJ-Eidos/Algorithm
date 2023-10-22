491f379da9ddf64d52cf905fafcd5acd5dba1268
JYJ <121663316+JYJ-Eidos@users.noreply.github.com>
2023-10-22 18:14:38 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.71 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -0,0 +1,6 @@
+const solution = (A, B) => {
+  const sortA = A.sort((a, b) => a - b);
+  const sortB = B.sort((a, b) => b - a);
+
+  return sortA.reduce((acc, cur, idx) => acc + cur * sortB[idx], 0);
+};
