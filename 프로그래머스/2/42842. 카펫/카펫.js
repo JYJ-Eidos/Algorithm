959c788ad744748398a056d7a978fa7b38a82227
JYJ <121663316+JYJ-Eidos@users.noreply.github.com>
2023-11-22 12:38:10 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -0,0 +1,7 @@
+const solution = (brown, yellow) => {
+  for (let i = 1; i <= yellow; i++) {
+    if (yellow % i === 0 && (yellow / i) * 2 + i * 2 + 4 === brown) {
+      return [yellow / i + 2, i + 2];
+    }
+  }
+};
