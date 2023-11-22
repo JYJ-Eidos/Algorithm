fc0af03bb392be215b08b417bc2e7f27be561147
JYJ <121663316+JYJ-Eidos@users.noreply.github.com>
2023-11-22 12:54:32 +0900

    [level unrated] Title: 유한소수 판별하기, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -0,0 +1,17 @@
+const solution = (a, b) => {
+  for (let i = a; i >= 2; i--) {
+    if (a % i === 0 && b % i === 0) {
+      a /= i;
+      b /= i;
+    }
+  }
+
+  while (b % 2 === 0) {
+    b /= 2;
+  }
+  while (b % 5 === 0) {
+    b /= 5;
+  }
+
+  return b === 1 ? 1 : 2;
+};
