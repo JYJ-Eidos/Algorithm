ddc37d2f2a8b79e1e9835dd762a8607974a4ba28
JYJ <121663316+JYJ-Eidos@users.noreply.github.com>
2023-11-23 14:34:14 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,22 +1,6 @@
 const solution = (n, arr1, arr2) => {
-  const addArr = arr1.map((el, idx) => {
-    let returnValue = "";
-
-    const addMap = String(
-      Number(el.toString(2).padStart(n, "0")) +
-        Number(arr2[idx].toString(2).padStart(n, "0"))
-    ).padStart(n, "0");
-
-    for (let i = 0; i < n; i++) {
-      if (addMap[i] === "0") {
-        returnValue += " ";
-      } else {
-        returnValue += "#";
-      }
-    }
-
-    return returnValue;
+  return arr1.map((el, idx) => {
+    const combined = (el | arr2[idx]).toString(2).padStart(n, "0");
+    return combined.replace(/1/g, "#").replace(/0/g, " ");
   });
-
-  return addArr;
 };
