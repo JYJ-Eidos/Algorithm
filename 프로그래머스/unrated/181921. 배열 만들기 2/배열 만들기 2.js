9b3ae0730184e0991e5681e2d89000df8c88e237
JYJ <121663316+JYJ-Eidos@users.noreply.github.com>
2023-11-25 21:29:33 +0900

    [level unrated] Title: 배열 만들기 2, Time: 6.31 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
@@ -1,16 +1,9 @@
 const solution = (l, r) => {
-  const binaryPattern = /^[01]+$/;
+  const binaryPattern = /^[05]+$/;
   const answer = Array(r - l + 1)
     .fill(l)
     .map((num, idx) => num + idx)
-    .filter((num) => {
-      if (num % 5 === 0) {
-        if (binaryPattern.test(String(num / 5))) {
-          return true;
-        }
-      }
-      return false;
-    });
+    .filter((num) => binaryPattern.test(String(num)));
 
   return answer.length === 0 ? [-1] : answer;
 };
